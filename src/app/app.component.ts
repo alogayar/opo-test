277d05a14dae05a65533163862fc73e113a05ae6
alogayar <alogayar@gmail.com>
2022-10-12 15:57:00 +0200

    leer fichero test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  title = 'OPO TEST';
-
-
+export class AppComponent implements OnInit {
 
   
+
+
+  ngOnInit(): void {
+    document
+      .getElementById('file-input')
+      .addEventListener('change', this.leerArchivo, false);
+  }
+
+  leerArchivo(event) {    
+    const file = event.target.files[0];    
+    const reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = (event) => {
+      const result = reader.result;      
+      const lineas = result.toString().split('\n')      
+      console.log(lineas);
+      document
+      .getElementById('contenido-archivo').innerHTML = lineas.toString();
+
+
+    };
+  }
+  
 }
+
+

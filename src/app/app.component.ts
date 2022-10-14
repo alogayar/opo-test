ccf90d73a67db6a97a00253d2b8a9fc65e5af1bd
DJ12571J <aogayar@dipujaen.es>
2022-10-15 01:48:06 +0200

    Validacion y mejora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,21 @@ export class AppComponent implements AfterViewChecked {
 
   test: any = null;
   pregunta: any = {};
-  preguntas: any[] = [];
-  preguntadas: number[] = [];
-  seleccionada: string;
-  
+
+  contador: number = 0;
+
+  preguntas: any[] = []; //array con todas las preguntas
+  preguntadas: number[] = []; // array para guardar los numeros de las preguntas
+  seleccionada: string = null; // opcion radiobuton
+  examen: any[] = []; // array con 40 preguntas;
+  respuestas: number[] = []; // array
+
+  private disabled = true;
+
   @ViewChild('inputFichero') _input: ElementRef;
   @ViewChild('biniciar') _iniciar: ElementRef;
   @ViewChild('body') _body: ElementRef;
+
 
 
   constructor() {
@@ -31,12 +39,12 @@ export class AppComponent implements AfterViewChecked {
       let pregunta = {};
       for (let i = 0, j = 0; i < this.test.length; i++, j++) {
         if (j > 5) j = 0;
-        if (j == 0) pregunta['pregunta'] = this.test[i];
-        if (j == 1) pregunta['respuesta1'] = this.test[i];
-        if (j == 2) pregunta['respuesta2'] = this.test[i];
-        if (j == 3) pregunta['respuesta3'] = this.test[i];
-        if (j == 4) pregunta['respuesta4'] = this.test[i];
-        if (j == 5) pregunta['correcta'] = this.test[i].replace('\r','');
+        if (j == 0) pregunta['pregunta'] = this.test[i].replace('\r', '');
+        if (j == 1) pregunta['respuesta1'] = this.test[i].replace('\r', '');
+        if (j == 2) pregunta['respuesta2'] = this.test[i].replace('\r', '');
+        if (j == 3) pregunta['respuesta3'] = this.test[i].replace('\r', '');
+        if (j == 4) pregunta['respuesta4'] = this.test[i].replace('\r', '');
+        if (j == 5) pregunta['correcta'] = this.test[i].replace('\r', '');
         if (j == 5) {
           this.preguntas.push(pregunta);
           pregunta = {};
@@ -56,50 +64,71 @@ export class AppComponent implements AfterViewChecked {
     reader.readAsText(fichero);
     reader.onload = () => {
       res = reader.result;
-      this.test = res.split('\n');
+      this.test = res.replace('\r', '').split('\n');
     };
   }
 
   getPregunta() {
-    const aleatorio = this.getAleatorio(this.preguntas.length)
-    this.preguntadas.push(aleatorio);
-    return this.preguntas[aleatorio];
+    this.disabled = true;
+    if (this.contador < 40) {
+      const pregunta = this.preguntas[this.contador];
+      this.contador++;
+      return pregunta;
+    }
   }
 
-  iniciar(){
-    this._iniciar.nativeElement.hidden = true;
+  siguientePregunta() {
+    this.contador++;
+    this.seleccionada = null;
+    this._body.nativeElement.style.backgroundColor = '#fff';
     this.pregunta = this.getPregunta();
   }
 
-  validar(){
+  // Crea un examen
+  creaNumerodePreguntas() {
+    this._iniciar.nativeElement.hidden = true;
+    this.examen = [];
+    this.preguntadas = [];
+    for (let i = 0; i < 40; i++) {
+      let buscar = true;
+      let num = 0;
+      while (buscar) {
+        num = this.getAleatorio(this.preguntas.length);
+        buscar = this.preguntadas.includes(num);
+      }
+      this.preguntadas.push(num);
+    }
+  }
+
+  creaExamen() {
+    this.contador = 0;
+    if (!this.preguntadas.length) {
+      this.creaNumerodePreguntas()
+    }
+    this.pregunta = this.getPregunta();
+  }
+
+  validar() {
     console.log(this.pregunta)
+    if (!this.seleccionada) return;
     if (this.seleccionada.toString() === this.pregunta.correcta.toString()) {
       console.log('CORRECTA')
       this._body.nativeElement.style.backgroundColor = '#a8dd99';
+      this.respuestas[this.contador - 1] = 1;
     }
     else {
       console.log('FALLADA');
       this._body.nativeElement.style.backgroundColor = '#fca4b8';
-      
+      this.respuestas[this.contador - 1] = 0;
     }
+ 
+    this.disabled = false;
   }
 
 
 
 
 
-
-  // leerArchivo(event) {    
-  //   this.aux = 'valor 2';
-  //   const file = event.target.files[0];    
-  //   const reader = new FileReader();
-  //   reader.readAsText(file);
-
-  //   reader.onload = (event) => {
-  //     const result = reader.result;            
-  //     this.texto = 'dsfd';
-  //   };
-  // }
 
 }
 
